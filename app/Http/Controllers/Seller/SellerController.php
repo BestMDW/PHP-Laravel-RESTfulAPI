e0eb16da7f1624a685503ed0e43bab5081a9ce6d
Maciej <bestmdw@gmail.com>
2017-10-22 23:08:06 +0100

    Buyer Controller and Seller Controller finished.

--- app/Http/Controllers/Seller/SellerController.php
+++ app/Http/Controllers/Seller/SellerController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Seller;
 
-use Illuminate\Http\Request;
+use App\Seller;
 use App\Http\Controllers\Controller;
 
 class SellerController extends Controller
@@ -14,29 +14,14 @@ class SellerController extends Controller
      */
     public function index()
     {
-        //
+        // Find sellers (users with at least one product).
+        $sellers = Seller::has('products')->get();
+
+        // Convert output to JSON with 200 [OK] status.
+        return response()->json(['data' => $sellers], 200);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+    /******************************************************************************************************************/
 
     /**
      * Display the specified resource.
@@ -46,40 +31,10 @@ class SellerController extends Controller
      */
     public function show($id)
     {
-        //
-    }
+        // Find seller (user with at least one product) with specific ID.
+        $seller = Seller::has('products')->findOrFail($id);
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        // Convert output to JSON with 200 [OK] status.
+        return response()->json(['data' => $seller], 200);
     }
 }
